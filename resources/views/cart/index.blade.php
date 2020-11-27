2d82e6535f831d0b5cd78f595f8ce5915efbf593
RISMOO <maurice.sider29@gmail.com>
2020-11-27 07:20:08 +0100

    commit

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -14,7 +14,7 @@
     <div class="pb-5">
       <div class="container">
         <div class="row">
-          <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+          <div class="col-lg-12 p-5 bg-white rounded shadow-sm mb-4">
 
             <!-- Shopping cart table -->
             <div class="table-responsive">
